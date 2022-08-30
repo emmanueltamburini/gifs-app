ed94ee23202ec878d8dc604a12d716dcc27e0814
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2022-08-30 18:29:43 -0400

    validation in record has been added

---
+++ gifsApp/src/app/gifs/constant/constant.ts
@@ -0,0 +1,1 @@
+export const MAX_RECORD_NUMBER: number = 10;
--- gifsApp/src/app/gifs/serivces/gifs.service.ts
+++ gifsApp/src/app/gifs/serivces/gifs.service.ts
@@ -11,8 +11,36 @@ export class GifsService {
     return [...this._record];
   }
 
-  searchGifs(query: string) {
-    this._record.unshift(query);
+  public searchGifs(query: string) {
+    if (this.validateQuery(query)) return;
+
+    this.insertRecord(query);
+
     console.log(this._record);
+  }
+
+  private validateQuery(query: string): boolean {
+    return query.trim().length === 0;
+  }
+
+  private cutRecord(): void {
+    this._record = this._record.splice(0, 10);
+  }
+
+  private setFormatQuery(query: string): string {
+    return query.trim().toLowerCase();
+  }
+
+  private insertRecord(query: string): void {
+    const index = this._record.indexOf(query);
+    const formatQuery = this.setFormatQuery(query);
+
+    if (index !== -1) {
+      this._record.splice(index, 1);
+      this._record.unshift(formatQuery);
+    } else {
+      this._record.unshift(formatQuery);
+      this.cutRecord();
+    }
   }
 }
